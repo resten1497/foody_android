e3e4476f37b2c584226914198c1f2698d0459bb1
GingGingI <sma0147@naver.com>
2019-07-30 18:16:47 +0900

    fontBug fixed

--- app/src/main/java/c/gingdev/foody_android/MainActivity.kt
+++ app/src/main/java/c/gingdev/foody_android/MainActivity.kt
@@ -2,11 +2,33 @@ package c.gingdev.foody_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import c.gingdev.foody_android.databinding.ActivityMainBinding
+import c.gingdev.foody_android.vm.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val model = MainActivityViewModel(applicationContext)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding
+                = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        binding.run {
+            model = this@MainActivity.model
+                .also { it.onCreate() }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        model.onStart()
+    }
+    override fun onPause() {
+        super.onPause()
+        model.onPaused()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        model.onDestroy()
     }
 }
